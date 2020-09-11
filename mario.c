4b65a86a69804ab88151244dbf9895f3758f87e7
Sarah-May Archibald <sarahmay.archibald@gmail.com>
2020-09-11 17:22:48 +0100

    adding Mario pset

---
+++ mario.c
@@ -0,0 +1,30 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+//declaring an integer
+int n;
+//do while loop prompting the user
+do
+{
+n = get_int("Height: ");
+}
+while (n < 1 || n > 8);
+//Two nested for loops for height and width
+for (int i = 0; i < n; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        if (i + j >= n - 1)
+        {
+        printf ("#");
+        }
+        else
+        {
+        printf(" ");
+        }
+    }
+    printf("\n");
+}
+}
